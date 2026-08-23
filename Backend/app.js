914a52fa211d97cd4429f4c6f5bf80b0bbf1f0c8
Rajat Sahani <rajatsahani272@gmail.com>
2026-08-24 00:03:32 +0530

    fix routing

--- Backend/app.js
+++ Backend/app.js
@@ -25,7 +25,8 @@ const allowedOrigins = [
     "https://ai-web3-futuretech-hackathon-2026-l.vercel.app",
 
     // User Frontend
-    "https://ai-web3-futuretech-hackathon-2026-l-two.vercel.app"
+    "https://ai-web3-futuretech-hack-git-1d6631-rajatsahani272-bots-projects.vercel.app",
+    "https://ai-web3-futuretech-hackathon-2026-lucknow-aegisai-orsmp6pe4.vercel.app",
 
 ];
 app.use(
